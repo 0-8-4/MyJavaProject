9556c6b67e5b16be83a0ba2c749d29895ce121d8
0-8-4 <ljd69154@liangjundi.cn>
2019-09-16 00:43:23 +0800

    Initial commit

---
+++ src/lab1/Homework.java
@@ -0,0 +1,5 @@
+package lab1;
+
+public class Homework {
+    //TODO
+}
---
+++ src/lab1/MyFirstProgram.java
@@ -0,0 +1,33 @@
+package lab1;
+
+public class MyFirstProgram {
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("My Java Program Running");
+        System.out.println("My name is Jundi Liang");
+
+        int a = 5;
+        int b = 8;
+        float f = 8.0f;
+        boolean c = true;
+        boolean d = false;
+
+        System.out.println("b value is " + b);
+        System.out.println("b/2 value is " + b / 2);
+
+        b++;
+
+        System.out.println("b value is " + b);
+        System.out.println("b/2 value is " + b / 2);
+        System.out.println("f value is " + f);
+        System.out.println("f/2 value is " + f / 2);
+
+        f++;
+
+        System.out.println("f value is " + f);
+        System.out.println("f/2 value is " + f / 2);
+
+
+    }
+}
